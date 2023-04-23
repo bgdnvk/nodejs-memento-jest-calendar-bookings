e39703ed172e5c0c5cb58fbea49352d51e396fe4
bgdnvk <bnovykov@gmail.com>
2023-04-23 21:00:50 +0200

    extracting n comments

--- Calendar.js
+++ Calendar.js
@@ -7,6 +7,7 @@ function calendarJSON(calendarNumber) {
 	 return JSON.parse(rawdata)
 }
 
+//free slots that are not in conflict with current sessions
 function getAvailableSlots(data, date) {
 
     const slots = data.slots[date] || []
@@ -35,6 +36,55 @@ function getAvailableSlots(data, date) {
         })
     })
 }
+
+//JSDoc example that I mentioned: https://jsdoc.app/ (some people use this instead of TS)
+/**
+* returns a formatted arr with available slots considering the duration of the session and the time you need before and after
+* @param {Array} realSpots - An array of available time slots.
+* @param {string} dateISO - The date in ISO format (YYYY-MM-DD).
+* @param {number} durationBefore - The duration before the appointment.
+* @param {number} duration - The duration of the appointment.
+* @param {number} durationAfter - The duration after the appointment.
+* @returns {Array} An array of available time slots.
+*/
+function getSlots(realSpots, dateISO, durationBefore, duration, durationAfter) {
+    //will return this after storing the data
+    const arrSlot = []
+    //check all the time slots and see if given the duration you will have enough time
+    realSpots.forEach((slot) => {
+        //get utc times
+        //we will be updating start at the end as this will be our time tracker
+        let start = moment.utc(dateISO + ' ' + slot.start)
+        const end = moment.utc(dateISO + ' ' + slot.end)
+        //loop to check if the duration fits the time slot
+        while (start.isBefore(end)) {
+            console.log('--------------------------------------------------WHILE------------------------------------------------')
+            console.log('start', start)
+            //get start and end times with duration of the appointment
+            const clientStartHour = start.clone().add(durationBefore, 'minutes')
+            const clientEndHour = clientStartHour.clone().add(duration, 'minutes')
+            const endHour = clientEndHour.clone().add(durationAfter, 'minutes')
+            //if the time given doesn't fit the appointment aka the appointment goes beyond the end time of the slot brea 
+            if (endHour.isAfter(end)) {
+                break
+            }
+            //since we don't have any conflicts we push the data
+            arrSlot.push({
+                startHour: start.toDate(),
+                endHour: endHour.toDate(),
+                clientStartHour: clientStartHour.toDate(),
+                clientEndHour: clientEndHour.toDate(),
+            });
+            //time tracker update
+            //update start to the end time of the appointment/booking/exercise
+            start = endHour.clone()
+        }
+    })
+    //return the slots that will fit all the duration vars
+    return arrSlot
+}
+
+//final function to get all available spots with formatted data
 function getAvailableSpots(calendar, date, duration) {
     
     let data = calendarJSON(calendar)
@@ -55,64 +105,11 @@ function getAvailableSpots(calendar, date, duration) {
     // console.log('----- new algo')
     let availableSlots = []
 
-
     availableSlots = getAvailableSlots(data, date)
-    console.log('formatted:', availableSlots)
-
-    /**
-    * Returns an array of available time slots for a given date and duration.
-    * @param {Array} realSpots - An array of available time slots.
-    * @param {string} dateISO - The date in ISO format (YYYY-MM-DD).
-    * @param {number} durationBefore - The duration before the appointment.
-    * @param {number} duration - The duration of the appointment.
-    * @param {number} durationAfter - The duration after the appointment.
-    * @returns {Array} An array of available time slots.
-    */
-    function getSlots(realSpots, dateISO, durationBefore, duration, durationAfter) {
-        //will return this after storing the data
-        const arrSlot = []
-
-        //check all the time slots and see if given the duration you will have enough time
-        realSpots.forEach((slot) => {
-            //get utc times
-            //we will be updating start at the end as this will be our time tracker
-            let start = moment.utc(dateISO + ' ' + slot.start)
-            const end = moment.utc(dateISO + ' ' + slot.end)
-
-            //loop to check if the duration fits the time slot
-            while (start.isBefore(end)) {
-                console.log('--------------------------------------------------WHILE------------------------------------------------')
-                console.log('start', start)
-                //get start and end times with duration of the appointment
-                const clientStartHour = start.clone().add(durationBefore, 'minutes')
-                const clientEndHour = clientStartHour.clone().add(duration, 'minutes')
-                const endHour = clientEndHour.clone().add(durationAfter, 'minutes')
-
-                //if the time given doesn't fit the appointment aka the appointment goes beyond the end time of the slot brea 
-                if (endHour.isAfter(end)) {
-                    break
-                }
-
-                //since we don't have any conflicts we push the data
-                arrSlot.push({
-                    startHour: start.toDate(),
-                    endHour: endHour.toDate(),
-                    clientStartHour: clientStartHour.toDate(),
-                    clientEndHour: clientEndHour.toDate(),
-                });
-
-                //time tracker update
-                //update start to the end time of the appointment/booking/exercise
-                start = endHour.clone()
-            }
-        });
-
-        //return the slots that will fit all the duration vars
-        return arrSlot
-    }
+    // console.log('formatted:', availableSlots)
 
     let arrSlot = getSlots(availableSlots, dateISO, durationBefore, duration, durationAfter)
-    console.log('REEEEEEEEEEEEEEEEEEEEEEEEEES', arrSlot)
+    // console.log('REEEEEEEEEEEEEEEEEEEEEEEEEES', arrSlot)
     return arrSlot
 }
 
